f3a4ddc89e5ff55c0306998f7ff9cfdced48310b
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-11-24 02:04:51 +0000

    Added stubs

--- src/download_data.py
+++ src/download_data.py
@@ -13,7 +13,8 @@ def retrieve_geojson(dataset_url, filename):
 def retrieve_defra_crow_data():
     """Downloads DEFRA CRoW open access land data.
     """
-    link = 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/CRoW_Act_2000_Access_Layer/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+    link = 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/CRoW_' \
+    'Act_2000_Access_Layer/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
     retrieve_geojson(link, 'defra_crow_oa.geojson')
 
 
@@ -23,8 +24,23 @@ def retrieve_national_trust_data():
     This includes land open due to CRoW or permission from the NT (2023).
 
     """
-    link = 'https://services-eu1.arcgis.com/NPIbx47lsIiu2pqz/arcgis/rest/services/National_Trust_Open_Data_Land_Always_Open/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+    link = 'https://services-eu1.arcgis.com/NPIbx47lsIiu2pqz/arcgis/rest/services/' \
+    'National_Trust_Open_Data_Land_Always_Open/FeatureServer/0/query?outFields=*&' \
+    'where=1%3D1&f=geojson'
     retrieve_geojson(link, 'national_trust_oa.geojson')
+
+
+def retrieve_right_of_way_data():
+    pass
+
+
+def retrieve_local_authority_polygons():
+    pass
+
+
+def retrieve_postcode_polygons():
+    pass
+
 
 if __name__ == '__main__':
     retrieve_defra_crow_data()
