9b147afd414f525b85632d28c1ce90285e4b162b
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-11-25 23:28:13 +0000

    Postcode data processing function added

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,8 @@
 import os
 import zipfile
 import requests
+import pandas as pd
+import geopandas as gp
 
 
 def retrieve_geojson(dataset_url, filename):
@@ -23,6 +25,7 @@ def retrieve_defra_crow_data():
     """
     link = 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/CRoW_' \
     'Act_2000_Access_Layer/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+
     retrieve_geojson(link, 'defra_crow_oa.geojson')
 
 
@@ -35,6 +38,7 @@ def retrieve_national_trust_data():
     link = 'https://services-eu1.arcgis.com/NPIbx47lsIiu2pqz/arcgis/rest/services/' \
     'National_Trust_Open_Data_Land_Always_Open/FeatureServer/0/query?outFields=*&' \
     'where=1%3D1&f=geojson'
+
     retrieve_geojson(link, 'national_trust_oa.geojson')
 
 
@@ -47,12 +51,35 @@ def retrieve_local_authority_polygons():
     link = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/' \
     'LAU1_Dec_2015_GCB_in_England_and_Wales_2022/FeatureServer/0/query?outFields' \
     '=*&where=1%3D1&f=geojson'
+
     retrieve_geojson(link, 'local_authority_boundaries.geojson')
+
+
+def prepare_postcode_data():
+    df = pd.read_csv(
+        'data/ons_postcode_data/Data/ONSPD_FEB_2023_UK.csv',
+        low_memory=False
+    )
+
+    # Filter to England postcodes only.
+    df = df.loc[df['ctry'] == 'E92000001']
+    df = df[['pcd', 'pcd2', 'pcds', 'lat', 'long']]
+    df = df.reindex()
+
+    gdf = gp.GeoDataFrame(
+        df,
+        geometry=gp.points_from_xy(df.long, df.lat),
+        crs='EPSG:4326'
+    )
+
+    filepath = os.path.join('data', 'ons_postcode_data.geojson')
+    gdf.to_file(filepath, driver="GeoJSON")
 
 
 def retrieve_postcode_data():
     url = 'https://www.arcgis.com/sharing/rest/content/items/a2f8c9c5778a452bbf6' \
     '40d98c166657c/data'
+
     response = requests.get(url)
     filepath = os.path.join('data', 'ons_postcode_data.zip')
 
@@ -63,8 +90,13 @@ def retrieve_postcode_data():
         extraction_path = os.path.join('data', 'ons_postcode_data')
         zip_ref.extractall(extraction_path)
 
+    prepare_postcode_data()
     os.remove(filepath)
 
 
 if __name__ == '__main__':
+    retrieve_defra_crow_data()
+    retrieve_national_trust_data()
+    retrieve_right_of_way_data()
     retrieve_local_authority_polygons()
+    retrieve_postcode_data()
