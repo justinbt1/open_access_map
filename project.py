0f05cc4d5f6335d5574e05011023cba60c69012e
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-12-28 00:27:20 +0000

    Moved to Python project file for x-platform compatability

---
+++ project.py
@@ -0,0 +1,27 @@
+import pathlib
+import subprocess
+import click
+import src
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def setup():
+    pathlib.Path('data').mkdir(exist_ok=True)
+    pathlib.Path('outputs').mkdir(exist_ok=True)
+
+    subprocess.run('conda env create --file=environment.yaml')
+    print('Run conda activate oa_maps to activate environment')
+
+
+@cli.command()
+def download_data():
+    src.download_datasets.download_all_data()
+
+
+if __name__ == '__main__':
+    cli()
