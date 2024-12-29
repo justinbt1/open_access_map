c393ed4e7e4c71a137283b4a1ce477ea6727d6ff
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-12-29 21:45:36 +0000

    Changed from overlay to sjoin

--- src/postcode_oa_radius.py
+++ src/postcode_oa_radius.py
@@ -3,17 +3,27 @@ import geopandas as gp
 def calculate_postcode_oa():
     print('Loading crow')
     crow_gdf = gp.read_file('data/defra_crow_oa.geojson')
+    crow_gdf.crs = 'EPSG:4326'
+    crow_gdf = crow_gdf.to_crs(epsg=6933)
+
     print('loading postcode')
     postcode_gdf = gp.read_file('data/ons_postcode_data.geojson')
-    postcode_gdf['geometry'] = postcode_gdf['geometry'].buffer(1000)
+    postcode_gdf = postcode_gdf.to_crs(epsg=6933)
 
-    intersection_gdf = gp.overlay(
-        postcode_gdf.head(),
-        crow_gdf,
-        how="intersection"
-    )
+    mile_radius = [1, 2.5, 5, 7.5]
+    for meter_radius in [mile * 1609.344 for mile in mile_radius]:
+        postcode_gdf = postcode_gdf['geometry'].buffer(meter_radius)
 
-    print(intersection_gdf)
+        print('Calculating intersections')
+        intersection_gdf = gp.sjoin(
+            postcode_gdf,
+            crow_gdf,
+            how='left',
+            predicate='intersects'
+        )
+
+        print(intersection_gdf.area.to_list())
+
 
 if __name__ == '__main__':
     calculate_postcode_oa()
